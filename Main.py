3455fd80beed508f446c7353438e8264dfee455b
codemaster <velludom@gmail.com>
2019-06-19 20:43:21 -0300

    Implementação PSO, sem testes

--- Main.py
+++ Main.py
@@ -1,10 +1,18 @@
-import PSO
+from PSO import pso
 
 maxIt = 1000
 ac1 = 2
 ac2 = 2
-vMax = 0
-vMin = 0
+vMax = 5
+vMin = -5
 
 
-pso(maxIt, ac1, ac2, vMax, vMin)
+
+x, v, melhoresSolucoes, valoresMinimos, valoresMedios = pso(maxIt, ac1, ac2, vMax, vMin)
+
+print("\nResultados:")
+print("\nx:\n", x)
+print("\nv:\n", v)
+print("\nMelhores Soluções:\n", melhoresSolucoes)
+print("\nValores Minimos:", valoresMinimos)
+print("\nValores Médios:", valoresMedios)
